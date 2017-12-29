ab216e58ff6c979b547e2a7b601e77b3ec8555c5
Multifacio <Mfprojects@live.nl>
2017-12-29 22:14:36 +0100

    Added the role enum. Contains all roles except Coven expansion roles

---
+++ TownOfSalemCalculator/src/townofsalemcalculator/Role.java
@@ -0,0 +1,48 @@
+package townofsalemcalculator;
+
+/**
+ * All the different roles within Town Of Salem (which includes Town, Mafia and Neutral roles)
+ * @author Multifacio
+ * @version 1.1
+ * @since 2017-12-29
+ */
+public enum Role {
+    //All Town roles
+    Bodyguard,
+    Doctor,
+    Escort,
+    Investigator,
+    Jailor,
+    Lookout,
+    Mayor,
+    Medium,
+    Retributionist,
+    Sheriff,
+    Spy,
+    Transporter,
+    VampireHunter,
+    Veteran,
+    Vigilante,
+    
+    //All Mafia roles
+    Blackmailer,
+    Consigliere,
+    Consort,
+    Disguiser,
+    Forger,
+    Framer,
+    Godfather,
+    Janitor,
+    Mafioso,
+    
+    //All Neutral roles
+    Amnesiac,
+    Arsonist,
+    Executioner,
+    Jester,
+    SerialKiller,
+    Survivor,
+    Vampire,
+    Werewolf,
+    Witch;
+}
