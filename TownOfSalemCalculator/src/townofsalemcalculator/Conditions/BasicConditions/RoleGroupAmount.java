601efc256b397f346c179979ed31b9b0243c8e0b
Multifacio <Mfprojects@live.nl>
2017-12-30 21:05:22 +0100

    Changed naming of variable roleSelection in RoleGroupAmount

--- TownOfSalemCalculator/src/townofsalemcalculator/Conditions/BasicConditions/RoleGroupAmount.java
+++ TownOfSalemCalculator/src/townofsalemcalculator/Conditions/BasicConditions/RoleGroupAmount.java
@@ -14,20 +14,20 @@ import townofsalemcalculator.RoleSelecter;
  * @since 2017-12-30
  */
 public class RoleGroupAmount implements Condition {
-    private final List<RoleSelecter> roleSelection; //The list of Role Selecters in which roleGroup must appear (at least/at most) amount of times
-    private final RoleGroup roleGroup; //The role group that must appear in the roleSelection (at least/at most) amount of times
-    private final Comparison comparison; //Determines whether the roleGroup must appear at least, at most or an equal amount of times as amount in the roleSelection
-    private final int amount; //The amount of times roleGroup must appear (at least/at most) in the roleSelection
+    private final List<RoleSelecter> roleSelecters; //The list of Role Selecters in which roleGroup must appear (at least/at most) amount of times
+    private final RoleGroup roleGroup; //The role group that must appear in the roleSelecters (at least/at most) amount of times
+    private final Comparison comparison; //Determines whether the roleGroup must appear at least, at most or an equal amount of times as amount in the roleSelecters
+    private final int amount; //The amount of times roleGroup must appear (at least/at most) in the roleSelecters
     
     /**
      * Create a Role Group Amount condition
-     * @param roleSelection The list of Role Selecters in which roleGroup must appear (at least/at most) amount of times
-     * @param roleGroup The role group that must appear in the roleSelection (at least/at most) amount of times
-     * @param comparison Determines whether the roleGroup must appear at least, at most or an equal amount of times as amount in the roleSelection
-     * @param amount The amount of times roleGroup must appear (at least/at most) in the roleSelection
+     * @param roleSelecters The list of Role Selecters in which roleGroup must appear (at least/at most) amount of times
+     * @param roleGroup The role group that must appear in the roleSelecters (at least/at most) amount of times
+     * @param comparison Determines whether the roleGroup must appear at least, at most or an equal amount of times as amount in the roleSelecters
+     * @param amount The amount of times roleGroup must appear (at least/at most) in the roleSelecters
      */
-    public RoleGroupAmount(List<RoleSelecter> roleSelection, RoleGroup roleGroup, Comparison comparison, int amount) {
-        this.roleSelection = roleSelection;
+    public RoleGroupAmount(List<RoleSelecter> roleSelecters, RoleGroup roleGroup, Comparison comparison, int amount) {
+        this.roleSelecters = roleSelecters;
         this.roleGroup = roleGroup;
         this.comparison = comparison;
         this.amount = amount;
