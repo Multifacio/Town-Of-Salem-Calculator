e1048cbe74310b0e9762628661813d5d4e3764fb
Multifacio <Mfprojects@live.nl>
2017-12-30 17:39:02 +0100

    Added the Condition interface

---
+++ TownOfSalemCalculator/src/townofsalemcalculator/Conditions/Condition.java
@@ -0,0 +1,11 @@
+package townofsalemcalculator.Conditions;
+
+/**
+ * A Condition is a statement about the game
+ * @author Multifacio
+ * @version 1.0
+ * @since 2017-12-30
+ */
+public interface Condition {
+
+}
