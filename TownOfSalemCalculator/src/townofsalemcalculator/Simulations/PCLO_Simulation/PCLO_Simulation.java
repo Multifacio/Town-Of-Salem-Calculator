5a4ae7b1c7130d684e432b771a5d489f3745e61f
Multifacio <Mfprojects@live.nl>
2017-12-31 13:47:22 +0100

    Continued with the PLCO_Simulation class

--- TownOfSalemCalculator/src/townofsalemcalculator/Simulations/PCLO_Simulation/PCLO_Simulation.java
+++ TownOfSalemCalculator/src/townofsalemcalculator/Simulations/PCLO_Simulation/PCLO_Simulation.java
@@ -1,12 +1,16 @@
 package townofsalemcalculator.Simulations.PCLO_Simulation;
 
+import java.util.List;
+import townofsalemcalculator.Conditions.Condition;
+
 /**
- * A Prioritized Condition Lineair Optimization simulation. It will determine how likely it is that a condition is true, based
- * on other prioritized conditions.
+ * A Prioritized Condition Lineair Optimization simulation. It will determine how likely it is that a condition is true, based on other prioritized conditions.
  * @author Multifacio
  * @version 1.0
- * @since 2017-12-30
+ * @since 2017-12-31
  */
 public class PCLO_Simulation {
-    
+    public int doSimulation(Condition check, List<PrioritizedCondition> holds) {
+        
+    }
 }
