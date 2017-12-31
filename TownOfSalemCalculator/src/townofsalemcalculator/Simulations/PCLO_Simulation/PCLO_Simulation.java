7c33c2bc552687386d4580a7ea955eb30e5d738f
Multifacio <Mfprojects@live.nl>
2017-12-31 12:43:47 +0100

    Started with the PCLO_Simulation class (not finished yet)

---
+++ TownOfSalemCalculator/src/townofsalemcalculator/Simulations/PCLO_Simulation/PCLO_Simulation.java
@@ -0,0 +1,12 @@
+package townofsalemcalculator.Simulations.PCLO_Simulation;
+
+/**
+ * A Prioritized Condition Lineair Optimization simulation. It will determine how likely it is that a condition is true, based
+ * on other prioritized conditions.
+ * @author Multifacio
+ * @version 1.0
+ * @since 2017-12-30
+ */
+public class PCLO_Simulation {
+    
+}
