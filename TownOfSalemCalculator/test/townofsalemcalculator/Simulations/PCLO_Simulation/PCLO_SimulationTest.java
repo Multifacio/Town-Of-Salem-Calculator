cc229fbf28edfb8356d9a679261dc0376dbf5e36
Multifacio <Mfprojects@live.nl>
2018-01-01 14:22:12 +0100

    Started with the test class of the PCLO_Simulation

---
+++ TownOfSalemCalculator/test/townofsalemcalculator/Simulations/PCLO_Simulation/PCLO_SimulationTest.java
@@ -0,0 +1,18 @@
+package townofsalemcalculator.Simulations.PCLO_Simulation;
+
+import org.junit.Test;
+
+/**
+ * The PCLO Simulation Test
+ * @author Multifacio
+ * @version 1.0
+ * @since 2018-1-1
+ */
+public class PCLO_SimulationTest {
+
+    @Test
+    public void testWrongCheck() {
+
+    }
+    
+}
