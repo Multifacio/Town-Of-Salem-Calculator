14d998bd58dc997534e002b1f4e41f92cf057f09
Multifacio <Mfprojects@live.nl>
2018-01-05 22:28:23 +0100

    Added the GameModus enum

---
+++ TownOfSalemCalculator/src/townofsalemcalculator/GameModus.java
@@ -0,0 +1,13 @@
+package townofsalemcalculator;
+
+/**
+ * Contains all supported Game Modi
+ * @author Multifacio
+ * @version 1.0
+ * @since 2018-1-5
+ */
+public enum GameModus {
+    Classic,
+    RankedPractice,
+    OldRankedPractice;
+}
