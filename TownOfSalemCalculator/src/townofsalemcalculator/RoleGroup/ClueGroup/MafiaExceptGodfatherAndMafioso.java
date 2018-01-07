61f0c1acbf208d3c8707b487121881e91910a03f
Multifacio <Mfprojects@live.nl>
2018-01-07 23:00:39 +0100

    Updated .gitignore

---
+++ TownOfSalemCalculator/src/townofsalemcalculator/RoleGroup/ClueGroup/MafiaExceptGodfatherAndMafioso.java
@@ -0,0 +1,17 @@
+package townofsalemcalculator.RoleGroup.ClueGroup;
+
+import townofsalemcalculator.RoleGroup.DifferenceRoleGroup;
+import townofsalemcalculator.RoleGroup.MafiaGroup.MafiaGroupNonCoven;
+import townofsalemcalculator.RoleGroup.RoleGroup;
+
+/**
+ * The group consisting of all Mafia roles except the Mafioso and the Godfather
+ * @author Multifacio
+ * @version 1.0
+ * @since 2017-1-7
+ */
+public class MafiaExceptGodfatherAndMafioso extends DifferenceRoleGroup {   
+    public MafiaExceptGodfatherAndMafioso() {
+        super(new MafiaGroupNonCoven(), new GodfatherAndMafioso());
+    }
+}
