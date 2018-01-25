1b0742076f1cac33f3400004df70700c32d98833
Multifacio <Mfprojects@live.nl>
2018-01-25 16:24:20 +0100

    Added Keywords for each role

--- TownOfSalemCalculator/src/townofsalemcalculator/Role.java
+++ TownOfSalemCalculator/src/townofsalemcalculator/Role.java
@@ -8,41 +8,51 @@ package townofsalemcalculator;
  */
 public enum Role {
     //All Town roles
-    Bodyguard,
-    Doctor,
-    Escort,
-    Investigator,
-    Jailor,
-    Lookout,
-    Mayor,
-    Medium,
-    Retributionist,
-    Sheriff,
-    Spy,
-    Transporter,
-    VampireHunter,
-    Veteran,
-    Vigilante,
+    Bodyguard(new String[]{"BG", "Bodyguard"}),
+    Doctor(new String[]{"Dr", "Doctor"}),
+    Escort(new String[]{"Escort"}),
+    Investigator(new String[]{"Investigator"}),
+    Jailor(new String[]{"Jailor"}),
+    Lookout(new String[]{"Lookout"}),
+    Mayor(new String[]{"Mayor"}),
+    Medium(new String[]{"Medium"}),
+    Retributionist(new String[]{"Retributionist"}),
+    Sheriff(new String[]{"Sheriff"}),
+    Spy(new String[]{"Spy"}),
+    Transporter(new String[]{"Transporter"}),
+    VampireHunter(new String[]{"Vampire", "Hunter", "VH"}),
+    Veteran(new String[]{"Veteran"}),
+    Vigilante(new String[]{"Vigilante"}),
     
     //All Mafia roles
-    Blackmailer,
-    Consigliere,
-    Consort,
-    Disguiser,
-    Forger,
-    Framer,
-    Godfather,
-    Janitor,
-    Mafioso,
+    Blackmailer(new String[]{"Blackmailer", "BMer"}),
+    Consigliere(new String[]{"Consigliere"}),
+    Consort(new String[]{"Consort"}),
+    Disguiser(new String[]{"Disguiser"}),
+    Forger(new String[]{"Forger"}),
+    Framer(new String[]{"Framer"}),
+    Godfather(new String[]{"Godfather", "GF"}),
+    Janitor(new String[]{"Janitor"}),
+    Mafioso(new String[]{"Mafioso"}),
     
     //All Neutral roles
-    Amnesiac,
-    Arsonist,
-    Executioner,
-    Jester,
-    SerialKiller,
-    Survivor,
-    Vampire,
-    Werewolf,
-    Witch;
+    Amnesiac(new String[]{"Amnesiac"}),
+    Arsonist(new String[]{"Arsonist"}),
+    Executioner(new String[]{"Executioner"}),
+    Jester(new String[]{"Jester"}),
+    SerialKiller(new String[]{"Serial", "Killer", "SK"}),
+    Survivor(new String[]{"Survivor"}),
+    Vampire(new String[]{"Vampire"}),
+    Werewolf(new String[]{"Werewolf", "WW"}),
+    Witch(new String[]{"Witch"});
+    
+    private final String[] keyWords;
+    
+    private Role(String[] keyWords) {
+        this.keyWords = keyWords;
+    }
+    
+    public String[] getKeyWords() {
+        return keyWords;
+    }
 }
