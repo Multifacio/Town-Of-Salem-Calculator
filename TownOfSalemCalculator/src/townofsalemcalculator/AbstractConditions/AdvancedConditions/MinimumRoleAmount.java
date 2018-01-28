4b011b3dbf12772a2b6e43b75356b50e8571f175
Multifacio <Mfprojects@live.nl>
2018-01-28 19:07:16 +0100

    Created an extra constructor for the MinimumRoleAmount

--- TownOfSalemCalculator/src/townofsalemcalculator/AbstractConditions/AdvancedConditions/MinimumRoleAmount.java
+++ TownOfSalemCalculator/src/townofsalemcalculator/AbstractConditions/AdvancedConditions/MinimumRoleAmount.java
@@ -5,8 +5,8 @@ import static townofsalemcalculator.Comparison.GreaterOrEqual;
 import townofsalemcalculator.AbstractConditions.BasicConditions.RoleGroupAmount;
 import townofsalemcalculator.Role;
 import townofsalemcalculator.RoleGroup.SingleRoleGroup;
-import townofsalemcalculator.Game.RoleSelecter;
 import townofsalemcalculator.Game.StartCategory;
+import townofsalemcalculator.RoleGroup.RoleGroup;
 
 /**
  * The Condition that a Role must occur a minimum amount of times. For example that there exist a Serial Killer in the game.
@@ -26,4 +26,13 @@ public class MinimumRoleAmount extends RoleGroupAmount {
         super(startCategories, new SingleRoleGroup(role), GreaterOrEqual, amount);
     }
     
+    /**
+     * Create a Minimum Role Amount condition
+     * @param startCategories The list of all Start Categories in a game
+     * @param roleGroup The roleGroup that must occur at least amount times in the game
+     * @param amount The amount of times role must occur at least in the game
+     */
+    public MinimumRoleAmount(List<StartCategory> startCategories, RoleGroup roleGroup, int amount) {
+        super(startCategories, roleGroup, GreaterOrEqual, amount);
+    }
 }
