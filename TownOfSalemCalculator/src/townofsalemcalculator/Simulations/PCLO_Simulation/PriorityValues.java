e1318858bfab020e82b0805f36702b79b94a2462
Multifacio <Mfprojects@live.nl>
2018-02-11 23:11:30 +0100

    Added some Priority Value documentation and added new priority values

--- TownOfSalemCalculator/src/townofsalemcalculator/Simulations/PCLO_Simulation/PriorityValues.java
+++ TownOfSalemCalculator/src/townofsalemcalculator/Simulations/PCLO_Simulation/PriorityValues.java
@@ -1,7 +1,10 @@
 package townofsalemcalculator.Simulations.PCLO_Simulation;
 
+//Priority Values can be obtained by the folowing formula: -4 * Ln(A), where A is the probability that the statement is not true.
 public enum PriorityValues {
-    TOP_PRIORITY(10000.0);
+    TOP_PRIORITY(10000.0),
+    INVESTIGATOR_NORMAL_PRIORITY(12.0),
+    SHERIFF_MEMBER_OF_MAFIA_PRIORITY(10.5);
     
     private final double value;
     
@@ -13,3 +16,5 @@ public enum PriorityValues {
         return value;
     }
 }
+
+
