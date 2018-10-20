41ee93bfa5a71eeb39000ec943b64ad9fda30935
Multifacio <Mfprojects@live.nl>
2018-10-20 18:13:42 +0200

    Added more priority values

--- TownOfSalemCalculator/src/townofsalemcalculator/Simulations/PCLO_Simulation/PriorityValues.java
+++ TownOfSalemCalculator/src/townofsalemcalculator/Simulations/PCLO_Simulation/PriorityValues.java
@@ -4,7 +4,9 @@ package townofsalemcalculator.Simulations.PCLO_Simulation;
 public enum PriorityValues {
     TOP_PRIORITY(10000.0),
     INVESTIGATOR_NORMAL_PRIORITY(12.0),
-    SHERIFF_MEMBER_OF_MAFIA_PRIORITY(10.5);
+    INVESTIGATOR_FRAMER_PRIORITY(10.5),
+    SHERIFF_MEMBER_OF_MAFIA_PRIORITY(10.5),
+    SHERIFF_OTHER_PRIORITY(12.0);
     
     private final double value;
     
