eee33a02b6a36cb7594701817514596fe8162b03
Multifacio <Mfprojects@live.nl>
2018-10-20 18:11:14 +0200

    Added Investigator conditions

---
+++ TownOfSalemCalculator/src/townofsalemcalculator/ConcreteConditions/InvestigatorMessages/ConcreteConditionInvestigatorInvestigatorConsigliereMayor.java
@@ -0,0 +1,54 @@
+package townofsalemcalculator.ConcreteConditions.InvestigatorMessages;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import townofsalemcalculator.AbstractConditions.AbstractCondition;
+import townofsalemcalculator.AbstractConditions.AdvancedConditions.EitherRoleForRoleSelecter;
+import townofsalemcalculator.ConcreteConditions.ConcreteCondition;
+import townofsalemcalculator.Game.AmnesiacDetermine;
+import townofsalemcalculator.Game.Player;
+import townofsalemcalculator.Role;
+import static townofsalemcalculator.Role.Consigliere;
+import static townofsalemcalculator.Role.Investigator;
+import static townofsalemcalculator.Role.Mayor;
+import townofsalemcalculator.RoleGroup.ClueGroup.InvestigatorClueGroup.InvestigatorClueInvestigatorConsigliereMayor;
+import townofsalemcalculator.Simulations.PCLO_Simulation.PrioritizedCondition;
+import static townofsalemcalculator.Simulations.PCLO_Simulation.PriorityValues.INVESTIGATOR_NORMAL_PRIORITY;
+
+/**
+ * The Concrete Condition that the Investigator sees that someone came up as Investigator, Consigliere or Mayor
+ * @author Multifacio
+ * @version 1.0
+ * @since 2017-2-11
+ */
+public class ConcreteConditionInvestigatorInvestigatorConsigliereMayor implements ConcreteCondition{
+    private final Player player;
+    
+    public ConcreteConditionInvestigatorInvestigatorConsigliereMayor(Player player) {
+        this.player = player;
+    }
+    
+    @Override
+    public String getCondition() {
+        return "Your target has sensitive information to reveal. They could be the Investigator, Consigliere or Mayor";
+    }
+
+    @Override
+    public List<String> keyWords() {
+        List<String> keyWords = Arrays.asList(new String[]{"Your", "Target", "Sensitive", "Information", "Reveal", "They"});
+        keyWords.addAll(Investigator.getKeyWords());
+        keyWords.addAll(Consigliere.getKeyWords());
+        keyWords.addAll(Mayor.getKeyWords());
+        return keyWords;
+    }
+
+    @Override
+    public PrioritizedCondition getPrioritizedCondition(List<ConcreteCondition> previousConditions) {
+        Set<Role> amnesiacTurnedInto = AmnesiacDetermine.getRememberedRoles(previousConditions); //Get all roles the Amnesiac has turned into
+        //The condition which needs to hold
+        AbstractCondition hold = new EitherRoleForRoleSelecter(player, new InvestigatorClueInvestigatorConsigliereMayor(amnesiacTurnedInto));
+        return new PrioritizedCondition(hold, INVESTIGATOR_NORMAL_PRIORITY.getValue());
+    }
+    
+}
---
+++ TownOfSalemCalculator/src/townofsalemcalculator/ConcreteConditions/InvestigatorMessages/ConcreteConditionInvestigatorSpyBlackmailerJailor.java
@@ -0,0 +1,55 @@
+package townofsalemcalculator.ConcreteConditions.InvestigatorMessages;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import townofsalemcalculator.AbstractConditions.AbstractCondition;
+import townofsalemcalculator.AbstractConditions.AdvancedConditions.EitherRoleForRoleSelecter;
+import townofsalemcalculator.ConcreteConditions.ConcreteCondition;
+import townofsalemcalculator.Game.AmnesiacDetermine;
+import townofsalemcalculator.Game.Player;
+import townofsalemcalculator.Role;
+import static townofsalemcalculator.Role.Blackmailer;
+import static townofsalemcalculator.Role.Investigator;
+import static townofsalemcalculator.Role.Jailor;
+import static townofsalemcalculator.Role.Spy;
+import townofsalemcalculator.RoleGroup.ClueGroup.InvestigatorClueGroup.InvestigatorClueSpyBlackmailerJailor;
+import townofsalemcalculator.Simulations.PCLO_Simulation.PrioritizedCondition;
+import static townofsalemcalculator.Simulations.PCLO_Simulation.PriorityValues.INVESTIGATOR_NORMAL_PRIORITY;
+
+/**
+ * The Concrete Condition that the Investigator sees that someone came up as Spy, Blackmailer or Jailor
+ * @author Multifacio
+ * @version 1.0
+ * @since 2017-2-12
+ */
+public class ConcreteConditionInvestigatorSpyBlackmailerJailor implements ConcreteCondition {
+    private final Player player;
+    
+    public ConcreteConditionInvestigatorSpyBlackmailerJailor(Player player) {
+        this.player = player;
+    }
+    
+    @Override
+    public String getCondition() {
+        return "Your target knows your darkest secrets. They could be the Spy, Blackmailer or Jailor";
+    }
+
+    @Override
+    public List<String> keyWords() {
+        List<String> keyWords = Arrays.asList(new String[]{"Your", "Target", "Knows", "Darkest", "Secrets", "They"});
+        keyWords.addAll(Investigator.getKeyWords());
+        keyWords.addAll(Spy.getKeyWords());
+        keyWords.addAll(Blackmailer.getKeyWords());
+        keyWords.addAll(Jailor.getKeyWords());
+        return keyWords;
+    }
+
+    @Override
+    public PrioritizedCondition getPrioritizedCondition(List<ConcreteCondition> previousConditions) {
+        Set<Role> amnesiacTurnedInto = AmnesiacDetermine.getRememberedRoles(previousConditions); //Get all roles the Amnesiac has turned into
+        //The condition which needs to hold
+        AbstractCondition hold = new EitherRoleForRoleSelecter(player, new InvestigatorClueSpyBlackmailerJailor(amnesiacTurnedInto));
+        return new PrioritizedCondition(hold, INVESTIGATOR_NORMAL_PRIORITY.getValue());
+    } 
+}
