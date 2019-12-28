156fdd0e38ded22b210ef628285072a0e023e5ac
Multifacio <Mfprojects@live.nl>
2019-12-28 23:43:03 +0100

    AtLeastCondition performance improvement by applying optimizations for same RoleCategories

--- src/Conditions/Abstract/AtLeastCondition.py
+++ src/Conditions/Abstract/AtLeastCondition.py
@@ -1,12 +1,17 @@
 from __future__ import annotations
+
+import math
+
 from src.Conditions.Condition import Condition
 from src.Concepts.Rolegroup import Rolegroup as RG
-from typing import Set, List, Tuple, TYPE_CHECKING, Union
+from typing import Set, List, Tuple, TYPE_CHECKING, Union, NamedTuple
 import itertools as it
 if TYPE_CHECKING:
     from src.Mechanics.Gamestate import Gamestate
     from src.Concepts.Role import Role
 
+AtLeastSelector = NamedTuple("AtLeastSelector", [("isPlayerRole", bool), ("index", Union[List[int], int]),
+                                                 ("multiplier", int)])
 class AtLeastCondition(Condition):
     """ The At Least Condition means that a set of roles should occur at least a given amount of times (n) between the
     players which means that n players should have one of the roles in this set of roles. """
@@ -25,56 +30,81 @@ class AtLeastCondition(Condition):
         return self.__valid_count(state) >= self.amount
 
     def inner_fill_evidence(self, state: Gamestate) -> List[Gamestate]:
-        # Check which category and game roles can be filled in with this condition.
-        options = []
+        # Check which category can be filled in with this condition
+        category_options = dict()
         for i, cr in enumerate(state.categoryRoles):
+            cr = frozenset(cr)
             if not self.roles.isdisjoint(cr):
-                options.append((False, i))
+                if cr in category_options:
+                    co = category_options[cr]
+                    category_options[cr] = AtLeastSelector(False, co.index + [i], co.multiplier + 1)
+                else:
+                    category_options[cr] = AtLeastSelector(False, [i], 1)
+
+        # Check which game roles can be filled in with this condition.
+        options = list(category_options.values())
         for i, pr in state.playerRoles.items():
             if pr is not None and not self.roles.isdisjoint(pr):
-                options.append((True, i))
+                options.append(AtLeastSelector(True, i, 1))
 
-        # Check if this condition is unsatisfiable and if so return an empty list
-        if len(options) < self.amount:
-            return []
-
-        # Loop through all possible new states
-        new_states = []
         opposite_roles = RG.NC_ALL.difference(self.roles)
-        for num in range(self.amount, len(options) + 1):
-            for comb in it.combinations(range(len(options)), num):
-                new_state = self.__state_combination(state, options, set(comb), opposite_roles)
-                if new_state is not None:
-                    new_states.append(new_state)
-
-        return new_states
+        return self.__all_combination(state, options, opposite_roles, [])
 
     def opposite(self) -> Condition:
         from src.Conditions.Abstract.AtMostCondition import AtMostCondition
         return AtMostCondition(self.roles, self.amount - 1)
 
-    def __state_combination(self, state: Gamestate, options: List[Tuple[bool, int]], combination: Set[int],
-                            opposite_roles: Set[Role]) -> Union[Gamestate, None]:
-        """ Compute the corresponding state for a given combination where the combination are all the indices in the
-        option list that should be a subset of the set of roles given for this condition. Returns None if the
-        combination is not valid. """
+    def __all_combination(self, state: Gamestate, options: List[AtLeastSelector], opposite_roles: Set[Role],
+                          cur_comb: List[int]) -> List[Gamestate]:
+        """ Determine all possible combinations for the given list of options (contains all possible category and player
+        roles that can be filled in). """
+        index = len(cur_comb)
+        optimistic_comb = cur_comb + [options[i].multiplier for i in range(index, len(options))]
+        optimistic_count = sum(optimistic_comb)
+        if optimistic_count < self.amount:
+            return []
+        else:
+            if index == len(options):
+                new_state = self.__state_combination(state, options, opposite_roles, cur_comb)
+                if new_state is None:
+                    return []
+                else:
+                    return [new_state]
+            else:
+                new_states = []
+                for i in range(options[index].multiplier + 1):
+                    new_states += self.__all_combination(state, options, opposite_roles, cur_comb + [i])
+                return new_states
+
+    def __state_combination(self, state: Gamestate, options: List[AtLeastSelector], opposite_roles: Set[Role],
+                            combination: List[int]) -> Union[Gamestate, None]:
+        """ Compute the corresponding state for a given combination where the combination indicates which options
+        should be selected. Returns None if the combination is not valid. """
         new_state = state.copy()
-        for i, option in enumerate(options):
-            intersect_roles = self.roles if i in combination else opposite_roles
-            is_player_role, index = option
-            if is_player_role:
-                new_roles = new_state.playerRoles[index].intersection(intersect_roles)
+        multiplier = 1
+        for num, option in zip(combination, options):
+            multiplier *= self.__nCr(option.multiplier, num)
+            if option.isPlayerRole:
+                i = option.index
+                new_roles = new_state.playerRoles[i].intersection(self.roles if num == 1 else opposite_roles)
                 if new_roles:
-                    new_state.playerRoles[index] = new_roles
+                    new_state.playerRoles[i] = new_roles
                 else:
                     return None
             else:
-                new_roles = new_state.categoryRoles[index].intersection(intersect_roles)
-                if new_roles:
-                    new_state.categoryRoles[index] = new_roles
-                else:
-                    return None
-
+                for i in option.index[:num]:
+                    new_roles = new_state.categoryRoles[i].intersection(self.roles)
+                    if new_roles:
+                        new_state.categoryRoles[i] = new_roles
+                    else:
+                        return None
+                for i in option.index[num:]:
+                    new_roles = new_state.categoryRoles[i].intersection(opposite_roles)
+                    if new_roles:
+                        new_state.categoryRoles[i] = new_roles
+                    else:
+                        return None
+        new_state.multiplier *= multiplier
         return new_state
 
     def __valid_count(self, state: Gamestate) -> int:
@@ -89,3 +119,7 @@ class AtLeastCondition(Condition):
                 valid_count += 1
 
         return valid_count
+
+    @staticmethod
+    def __nCr(n: int, k: int) -> int:
+        return math.factorial(n) // math.factorial(k) // math.factorial(n - k)
--- src/Mechanics/Gamestate.py
+++ src/Mechanics/Gamestate.py
@@ -1,11 +1,9 @@
 from __future__ import annotations
-from dataclasses import dataclass
 from typing import Set, List, Dict, Union
 from src.Concepts import Role
 from src.Conditions.Condition import Condition
 import math
 
-@dataclass
 class Gamestate:
     """ A current Gamestate after having set some/all of the player & category roles with the conditions that still
     should be applied on the Gamestate.
@@ -16,16 +14,35 @@ class Gamestate:
         playerRoles (Dict[int, Union[Set[Role], None]]): A dictionary where the keys are integers representing the
         player id and the values are possible roles which this player still can become. If a value for a given key is
         None then the player haven't yet selected a start category.
+        multiplier (int): How often this Gamestate counts (at the end of count_combinations function the result
+        will be multiplied by this multiplier value).
     """
-    categoryRoles: List[Set[Role]]
-    conditions: List[Condition]
-    playerRoles: Dict[int, Union[Set[Role], None]] = None
 
-    def __post_init__(self):
-        if self.playerRoles is None:
-            self.playerRoles = dict()
-            for i in range(1, len(self.categoryRoles) + 1):
-                self.playerRoles[i] = None
+    def __init__(self, categoryRoles: List[Set[Role]], conditions: List[Condition],
+                 playerRoles: Dict[int, Union[Set[Role], None]], multiplier: int):
+        """ Constructor of the Gamestate. Do not call this function! Call the create function if you want to create a
+        new Gamestate and call the copy function if you want to copy a Gamestate. """
+        self.categoryRoles = categoryRoles
+        self.conditions = conditions
+        self.playerRoles = playerRoles
+        self.multiplier = multiplier
+
+    @staticmethod
+    def create(game_modus: List[Set[Role]], condition: Condition) -> Gamestate:
+        """ Create a new fresh Gamestate based on a given Gamemodus with a condition that should hold on this Gamestate.
+
+        Arguments:
+            game_modus (List[Set[Role]]): The list with set of roles from which the player roles are being sampled.
+            condition (Condition): The condition that should hold on this Gamestate.
+
+        Returns:
+            The new fresh Gamestate with the given Gamemodus and the condition that should hold.
+        """
+        playerRoles = dict()
+        for i in range(1, len(game_modus) + 1):
+            playerRoles[i] = None
+
+        return Gamestate(game_modus, [condition], playerRoles, 1)
 
     def copy(self) -> Gamestate:
         """ Make a deep copy of this Gamesample object with the first condition removed.
@@ -36,7 +53,7 @@ class Gamestate:
         cr_copy = [set.copy() for set in self.categoryRoles]
         pr_copy = dict([(key, None if set is None else set.copy()) for key, set in self.playerRoles.items()])
         con_copy = self.conditions[1:]
-        return Gamestate(cr_copy, con_copy, pr_copy)
+        return Gamestate(cr_copy, con_copy, pr_copy, self.multiplier)
 
     def count_combinations(self) -> int:
         """ Count the number of possible Gamestates (with all values set) given the evidence.
@@ -63,6 +80,4 @@ class Gamestate:
         for pr in self.playerRoles.values():
             if pr is not None:
                 count *= len(pr)
-        return count
-
-
+        return self.multiplier * count
